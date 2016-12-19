fd467089d31349f2883c276504bcd0159fd4fdfd
Peeyar <peeyar2000@gmail.com>
2016-12-18 20:38:54 -0800

    My first commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+
+import java.io.*;
+import java.util.*;
+public class Test {
+	public static void main (String[] args) {
+		StringBuffer str1 = new StringBuffer ("My is Mike.");
+		String name = "name ";
+		str1.insert (3, name);
+		System.out.println (str1);
+		}
+}
